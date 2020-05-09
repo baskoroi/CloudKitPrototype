81354dcd46924a13a2cb1edca7675130a11ea16f
Baskoro Indrayana <baskoroindrayana@gmail.com>
2020-05-09 17:29:56 +0700

    Initial Commit

---
+++ CloudKitPrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudKitPrototype
+//
+//  Created by Baskoro Indrayana on 05/09/20.
+//  Copyright © 2020 Baskoro Indrayana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
